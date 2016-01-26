8373c4101131e2f211e3c18d1f75f3964b21eae0
thelowlypeon <peter@onedesigncompany.com>
2016-01-26 09:26:55 -0600

    removes vendor directory, now in gitignore

--- vendor/campaignmonitor/createsend-php/class/exceptions.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class CurlException extends \RuntimeException
-{
-    public function __construct($curlMessage, $errorCode)
-    {
-        parent::__construct('Error making request with curl_error: ' . $curlMessage, $errorCode);
-    }
-}
