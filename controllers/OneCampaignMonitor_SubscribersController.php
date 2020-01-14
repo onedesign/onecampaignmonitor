3a5f98fa632166afa68cd9249b1c17b05aaf539a
Michael Ramuta <michaelramuta@gmail.com>
2020-01-14 11:03:17 -0600

    update param name and remove unnecessary return

--- controllers/OneCampaignMonitor_SubscribersController.php
+++ controllers/OneCampaignMonitor_SubscribersController.php
@@ -10,12 +10,11 @@ class Onecampaignmonitor_SubscribersController extends BaseController
 
         // Verify the recaptcha response before creating the user
         if (craft()->plugins->getPlugin('OneCampaignMonitor')->getSettings()->verify_recaptcha) {
-            $response = craft()->request->getParam('recaptchaResult');
+            $response = craft()->request->getParam('g-recaptcha-response');
             $result = craft()->oneCampaignMonitor_recaptcha->verify($response);
             if (!$result['success']) {
                 $this->returnJson(['success' => false,
                                    'error' => 'Recaptcha Not Successful']);
-                return;
             }
         }
 
